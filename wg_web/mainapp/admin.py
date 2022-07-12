e36b92a60e2ef1fbb584507fa634210637f5996b
Alex <75622861+Geraldokz@users.noreply.github.com>
2022-07-12 15:13:37 +0700

    create models

--- wg_web/mainapp/admin.py
+++ wg_web/mainapp/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import VPNServer, VPNService, VPNClient, VPNDevice
+
+
+@admin.register(VPNServer)
+class VPNServerAdmin(admin.ModelAdmin):
+    list_display = ('hostname', 'public_ip', 'if_name')
+
+
+@admin.register(VPNService)
+class VPNServiceAdmin(admin.ModelAdmin):
+    list_display = ('service_name', 'vpn_network', 'vpn_port', 'private_ip', 'get_server_name', 'get_server_ip')
+
+    @admin.display(ordering='server_name', description='Server hostname')
+    def get_server_name(self, obj):
+        return obj.server.hostname
+
+    @admin.display(ordering='server_ip', description='Server IP')
+    def get_server_ip(self, obj):
+        return obj.server.public_ip
+
+
+@admin.register(VPNClient)
+class VPNClientAdmin(admin.ModelAdmin):
+    list_display = ('client_name', 'client_email', 'get_vpn_service_name')
+
+    @admin.display(ordering='client_name', description='VPN Service name')
+    def get_vpn_service_name(self, obj):
+        return obj.vpn_service.service_name
+
+
+@admin.register(VPNDevice)
+class VPNDeviceAdmin(admin.ModelAdmin):
+    list_display = ('device_name', 'device_type', 'private_ip', 'get_client_name')
+
+    @admin.display(ordering='client_name', description='Client name')
+    def get_client_name(self, obj):
+        return obj.client.client_name
--- wg_web/mainapp/models.py
+++ wg_web/mainapp/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+class VPNServer(models.Model):
+    """VPN Server model"""
+    hostname = models.CharField(max_length=255, verbose_name='Hostname')
+    public_ip = models.CharField(max_length=15, verbose_name='Server IP')
+    if_name = models.CharField(max_length=50, verbose_name='Eth interface name')
+
+    def __str__(self):
+        return f'{self.hostname}'
+
+
+class VPNService(models.Model):
+    """VPN Service model"""
+    service_name = models.CharField(max_length=255, verbose_name='VPN Service name')
+    vpn_network = models.CharField(max_length=18, verbose_name='VPN Service network')
+    vpn_port = models.IntegerField(verbose_name='VPN port')
+    private_ip = models.CharField(max_length=15, verbose_name='VPN Service ip')
+    vpn_if_name = models.CharField(max_length=50, verbose_name='VPN Service interface name', default='eth0')
+    public_key = models.CharField(max_length=44, verbose_name='VPN Service pubkey')
+    private_key = models.CharField(max_length=44, verbose_name='VPN Service privkey')
+    server = models.ForeignKey(VPNServer, on_delete=models.CASCADE, verbose_name='VPN server')
+
+    def __str__(self):
+        return f'{self.service_name}'
+
+
+class VPNClient(models.Model):
+    """VPN Client model"""
+    client_name = models.CharField(max_length=255, verbose_name='VPN Client name')
+    client_email = models.CharField(max_length=255, verbose_name='VPN Client email')
+    vpn_service = models.ForeignKey(VPNService, on_delete=models.CASCADE, verbose_name='VPN Service')
+
+    def __str__(self):
+        return f'{self.client_name}'
+
+
+class VPNDevice(models.Model):
+    """VPN Device model"""
+    DEVICE_TYPES = (
+        ('pc', 'PC'),
+        ('sm', 'Smartphone')
+    )
+
+    device_name = models.CharField(max_length=255, verbose_name='VPN Device name')
+    device_type = models.CharField(max_length=10, choices=DEVICE_TYPES, verbose_name='VPN Device type')
+    private_ip = models.CharField(max_length=15, verbose_name='VPN Device ip')
+    public_key = models.CharField(max_length=44, verbose_name='VPN Device pubkey')
+    private_key = models.CharField(max_length=44, verbose_name='VPN Device privkey')
+    client = models.ForeignKey(VPNClient, on_delete=models.CASCADE, verbose_name='VPN Client')
+
+    def __str__(self):
+        return f'{self.device_name}'
